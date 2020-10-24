d568e45c817c9c0d496dd399e9a4b36cc70bbcb8
Matheus Bach <math.bach.com@gmail.com>
2020-10-24 17:34:31 -0300

    NoYield changed to Yield

--- User/Settings.cs
+++ User/Settings.cs
@@ -75,7 +75,7 @@ namespace True_Mining_Desktop.User
         public List<string> AlgorithmsList { get; set; } = new List<string>();
         public int Priority { get; set; } = 1;
         public int MaxUsageHint { get; set; } = 100;
-        public bool NoYield { get; set; } = true;
+        public bool Yield { get; set; } = true;
     }
 
     public class NVIDIASettings
