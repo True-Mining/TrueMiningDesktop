8a91bb0f970576a28ba6bc1dafae9b7cb6d1728f
matheusbach <math.bach.com@gmail.com>
2021-12-05 02:02:40 -0300

    Update XMRig config

--- Core/Miners/XMRig.cs
+++ Core/Miners/XMRig.cs
@@ -255,60 +255,62 @@ namespace TrueMiningDesktop.Core.XMRig
             Server.MiningCoin miningCoin = SoftwareParameters.ServerConfig.MiningCoins.Find(x => x.Coin.Equals("xmr", StringComparison.OrdinalIgnoreCase));
 
             conf.AppendLine("{");
-            conf.AppendLine("   \"api\": {");
-            conf.AppendLine("       \"id\": null,");
-            conf.AppendLine("       \"worker-id\": null");
-            conf.AppendLine("   },");
-            conf.AppendLine("   \"http\": {");
-            conf.AppendLine("       \"enabled\": true,");
-            conf.AppendLine("       \"host\": \"" + (User.Settings.User.UseAllInterfacesInsteadLocalhost ? "0.0.0.0" : "127.0.0.1") + "\",");
-            conf.AppendLine("       \"port\": " + APIport + ",");
-            conf.AppendLine("       \"access-token\": null,");
-            conf.AppendLine("       \"restricted\": true");
-            conf.AppendLine("   },");
-            conf.AppendLine("   \"autosave\": false,");
-            conf.AppendLine("   \"colors\": true,");
-            conf.AppendLine("   \"title\": \"True Mining running XMRig\",");
-            conf.AppendLine("   \"cpu\": {");
-            conf.AppendLine("       \"enabled\": " + Settings.Device.cpu.MiningSelected.ToString().ToLowerInvariant() + ",");
-            conf.AppendLine("       \"huge-pages\": true,");
-            conf.AppendLine("       \"hw-aes\": null,");
-            if (!Settings.Device.cpu.Autoconfig) { conf.AppendLine("       \"priority\": " + Settings.Device.cpu.Priority + ","); } else { conf.AppendLine("       \"priority\": 1,"); }
-            conf.AppendLine("       \"memory-pool\": true,");
-            if (!Settings.Device.cpu.Autoconfig) { conf.AppendLine("       \"yield\": " + (Settings.Device.cpu.Yield).ToString().ToLowerInvariant() + ","); }
-            conf.AppendLine("       \"asm\": true,");
-            if (!Settings.Device.cpu.Autoconfig) { conf.AppendLine("       \"max-threads-hint\": " + Settings.Device.cpu.MaxUsageHint + ","); }
-            conf.AppendLine("       \"rx\": {");
-            conf.AppendLine("           \"init\": -1,");
-            conf.AppendLine("           \"mode\": \"auto\",");
-            conf.AppendLine("           \"1gb-pages\": true,");
-            conf.AppendLine("           \"rdmsr\": true,");
-            conf.AppendLine("           \"wrmsr\": true,");
-            conf.AppendLine("           \"cache_qos\": true,");
-            conf.AppendLine("           \"numa\": true,");
-            if (!Settings.Device.cpu.Autoconfig && Settings.Device.cpu.Threads > 0) { conf.AppendLine("           \"threads\": " + Settings.Device.cpu.Threads + ","); }
-            conf.AppendLine("       }");
-            conf.AppendLine("   },");
-            conf.AppendLine("   \"opencl\": {");
-            conf.AppendLine("       \"enabled\": " + Settings.Device.opencl.MiningSelected.ToString().ToLowerInvariant() + ",");
-            if (!Settings.Device.opencl.Autoconfig) { conf.AppendLine("       \"cache\": " + Settings.Device.opencl.Cache.ToString().ToLowerInvariant() + ","); }
-            conf.AppendLine("       \"loader\": null,");
-            conf.AppendLine("       \"platform\": \"AMD\",");
-            conf.AppendLine("       \"adl\": true,");
-            conf.AppendLine("       \"cn/0\": false,");
-            conf.AppendLine("       \"cn-lite/0\": false");
-            conf.AppendLine("   },");
-            conf.AppendLine("   \"cuda\": {");
-            conf.AppendLine("       \"enabled\": " + Settings.Device.cuda.MiningSelected.ToString().ToLowerInvariant() + ",");
-            conf.AppendLine("       \"loader\": null,");
-            if (!Settings.Device.cuda.Autoconfig) { conf.AppendLine("       \"nvml\": " + Settings.Device.cuda.NVML.ToString().ToLowerInvariant()); }
-            conf.AppendLine("   },");
-            conf.AppendLine("   \"donate-level\": 1,");
-            conf.AppendLine("   \"donate-over-proxy\": 1,");
-            conf.AppendLine("   \"log-file\": \"XMRig-log.txt\",");
-            conf.AppendLine("   \"retries\": 2,");
-            conf.AppendLine("   \"retry-pause\": 3,");
-            conf.AppendLine("   \"pools\": [");
+            conf.AppendLine("    \"api\": {");
+            conf.AppendLine("        \"id\": null,");
+            conf.AppendLine("        \"worker-id\": null");
+            conf.AppendLine("    },");
+            conf.AppendLine("    \"http\": {");
+            conf.AppendLine("        \"enabled\": true,");
+            conf.AppendLine("        \"host\": \"" + (User.Settings.User.UseAllInterfacesInsteadLocalhost ? "0.0.0.0" : "127.0.0.1") + "\",");
+            conf.AppendLine("        \"port\": " + APIport + ",");
+            conf.AppendLine("        \"access-token\": null,");
+            conf.AppendLine("        \"restricted\": true");
+            conf.AppendLine("    },");
+            conf.AppendLine("    \"autosave\": false,");
+            conf.AppendLine("    \"colors\": true,");
+            conf.AppendLine("    \"title\": \"True Mining running XMRig\",");
+            conf.AppendLine("    \"cpu\": {");
+            conf.AppendLine("        \"enabled\": " + Settings.Device.cpu.MiningSelected.ToString().ToLowerInvariant() + ",");
+            conf.AppendLine("        \"huge-pages\": true,");
+            conf.AppendLine("        \"hw-aes\": null,");
+            if (!Settings.Device.cpu.Autoconfig) { conf.AppendLine("        \"priority\": " + Settings.Device.cpu.Priority + ","); } else { conf.AppendLine("        \"priority\": 1,"); }
+            conf.AppendLine("        \"memory-pool\": true,");
+            if (!Settings.Device.cpu.Autoconfig) { conf.AppendLine("        \"yield\": " + (Settings.Device.cpu.Yield).ToString().ToLowerInvariant() + ","); }
+            conf.AppendLine("        \"asm\": true,");
+            if (!Settings.Device.cpu.Autoconfig && Settings.Device.cpu.Threads > 0) { conf.AppendLine("        \"threads\": " + Settings.Device.cpu.Threads + ","); }
+            if (!Settings.Device.cpu.Autoconfig) { conf.AppendLine("        \"max-threads-hint\": " + Settings.Device.cpu.MaxUsageHint); }
+            conf.AppendLine("    },");
+            conf.AppendLine("    \"randomx\": {");
+            conf.AppendLine("        \"init\": -1,");
+            conf.AppendLine("        \"init-avx2\": -1,");
+            conf.AppendLine("        \"mode\": \"auto\",");
+            conf.AppendLine("        \"1gb-pages\": true,");
+            conf.AppendLine("        \"rdmsr\": true,");
+            conf.AppendLine("        \"wrmsr\": true,");
+            conf.AppendLine("        \"cache_qos\": true,");
+            conf.AppendLine("        \"numa\": true,");
+            conf.AppendLine("        \"scratchpad_prefetch_mode\": true");
+            conf.AppendLine("    },");
+            conf.AppendLine("    \"opencl\": {");
+            conf.AppendLine("        \"enabled\": " + Settings.Device.opencl.MiningSelected.ToString().ToLowerInvariant() + ",");
+            if (!Settings.Device.opencl.Autoconfig) { conf.AppendLine("     \"cache\": " + Settings.Device.opencl.Cache.ToString().ToLowerInvariant() + ","); }
+            conf.AppendLine("        \"loader\": null,");
+            conf.AppendLine("        \"platform\": \"AMD\",");
+            conf.AppendLine("        \"adl\": true,");
+            conf.AppendLine("        \"cn/0\": false,");
+            conf.AppendLine("        \"cn-lite/0\": false");
+            conf.AppendLine("    },");
+            conf.AppendLine("    \"cuda\": {");
+            conf.AppendLine("        \"enabled\": " + Settings.Device.cuda.MiningSelected.ToString().ToLowerInvariant() + ",");
+            conf.AppendLine("        \"loader\": null,");
+            if (!Settings.Device.cuda.Autoconfig) { conf.AppendLine("        \"nvml\": " + Settings.Device.cuda.NVML.ToString().ToLowerInvariant()); }
+            conf.AppendLine("    },");
+            conf.AppendLine("    \"donate-level\": 0,");
+            conf.AppendLine("    \"donate-over-proxy\": 0,");
+            conf.AppendLine("    \"log-file\": \"XMRig-log.txt\",");
+            conf.AppendLine("    \"retries\": 2,");
+            conf.AppendLine("    \"retry-pause\": 3,");
+            conf.AppendLine("    \"pools\": [");
 
             List<string> addresses = miningCoin.Hosts;
 
@@ -344,40 +346,40 @@ namespace TrueMiningDesktop.Core.XMRig
             {
                 if (User.Settings.User.UseTorSharpOnMining)
                 {
-                    conf.AppendLine("       {");
-                    conf.AppendLine("           \"algo\": null,");
-                    conf.AppendLine("           \"coin\": \"monero\",");
-                    conf.AppendLine("           \"url\": \"" + host + ":" + miningCoin.StratumPort + "\",");
-                    conf.AppendLine("           \"user\": \"" + miningCoin.WalletTm + "." + Settings.User.PayCoin.CoinTicker.ToLowerInvariant() + '_' + Settings.User.Payment_Wallet + "/" + miningCoin.Email + "\", ");
-                    conf.AppendLine("           \"pass\": \"" + miningCoin.Password + "\",");
-                    conf.AppendLine("           \"rig-id\": null,");
-                    conf.AppendLine("           \"nicehash\": false,");
-                    conf.AppendLine("           \"keepalive\": false,");
-                    conf.AppendLine("           \"enabled\": true,");
-                    conf.AppendLine("           \"tls\": true,");
-                    conf.AppendLine("           \"tls-fingerprint\": null,");
-                    conf.AppendLine("           \"daemon\": false,");
-                    conf.AppendLine("           \"socks5\": \"127.0.0.1:8428\",");
-                    conf.AppendLine("           \"self-select\": null");
-                    conf.AppendLine("       },");
+                    conf.AppendLine("        {");
+                    conf.AppendLine("            \"algo\": null,");
+                    conf.AppendLine("            \"coin\": \"monero\",");
+                    conf.AppendLine("            \"url\": \"" + host + ":" + miningCoin.StratumPort + "\",");
+                    conf.AppendLine("            \"user\": \"" + miningCoin.WalletTm + "." + Settings.User.PayCoin.CoinTicker.ToLowerInvariant() + '_' + Settings.User.Payment_Wallet + "/" + miningCoin.Email + "\", ");
+                    conf.AppendLine("            \"pass\": \"" + miningCoin.Password + "\",");
+                    conf.AppendLine("            \"rig-id\": null,");
+                    conf.AppendLine("            \"nicehash\": false,");
+                    conf.AppendLine("            \"keepalive\": false,");
+                    conf.AppendLine("            \"enabled\": true,");
+                    conf.AppendLine("            \"tls\": true,");
+                    conf.AppendLine("            \"tls-fingerprint\": null,");
+                    conf.AppendLine("            \"daemon\": false,");
+                    conf.AppendLine("            \"socks5\": \"127.0.0.1:8428\",");
+                    conf.AppendLine("            \"self-select\": null");
+                    conf.AppendLine("        },");
                 }
 
-                conf.AppendLine("       {");
-                conf.AppendLine("           \"algo\": null,");
-                conf.AppendLine("           \"coin\": \"monero\",");
-                conf.AppendLine("           \"url\": \"" + host + ":" + miningCoin.StratumPort + "\",");
-                conf.AppendLine("           \"user\": \"" + miningCoin.WalletTm + "." + User.Settings.User.PayCoin.CoinTicker.ToLowerInvariant() + '_' + Settings.User.Payment_Wallet + "/" + miningCoin.Email + "\", ");
-                conf.AppendLine("           \"pass\": \"" + miningCoin.Password + "\",");
-                conf.AppendLine("           \"rig-id\": null,");
-                conf.AppendLine("           \"nicehash\": false,");
-                conf.AppendLine("           \"keepalive\": false,");
-                conf.AppendLine("           \"enabled\": true,");
-                conf.AppendLine("           \"tls\": true,");
-                conf.AppendLine("           \"tls-fingerprint\": null,");
-                conf.AppendLine("           \"daemon\": false,");
-                conf.AppendLine("           \"socks5\": null,");
-                conf.AppendLine("           \"self-select\": null");
-                conf.AppendLine("       },");
+                conf.AppendLine("        {");
+                conf.AppendLine("            \"algo\": null,");
+                conf.AppendLine("            \"coin\": \"monero\",");
+                conf.AppendLine("            \"url\": \"" + host + ":" + miningCoin.StratumPort + "\",");
+                conf.AppendLine("            \"user\": \"" + miningCoin.WalletTm + "." + User.Settings.User.PayCoin.CoinTicker.ToLowerInvariant() + '_' + Settings.User.Payment_Wallet + "/" + miningCoin.Email + "\", ");
+                conf.AppendLine("            \"pass\": \"" + miningCoin.Password + "\",");
+                conf.AppendLine("            \"rig-id\": null,");
+                conf.AppendLine("            \"nicehash\": false,");
+                conf.AppendLine("            \"keepalive\": false,");
+                conf.AppendLine("            \"enabled\": true,");
+                conf.AppendLine("            \"tls\": true,");
+                conf.AppendLine("            \"tls-fingerprint\": null,");
+                conf.AppendLine("            \"daemon\": false,");
+                conf.AppendLine("            \"socks5\": null,");
+                conf.AppendLine("            \"self-select\": null");
+                conf.AppendLine("        },");
             }
 
             conf.AppendLine("   ]");
