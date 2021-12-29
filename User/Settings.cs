c156d19c72fb59cf66064d601f3ce2483a6e281e
matheusbach <math.bach.com@gmail.com>
2021-12-29 18:54:38 -0300

    fix ocasional nulll Payment_Coin in userConfig

--- User/Settings.cs
+++ User/Settings.cs
@@ -99,6 +99,7 @@ namespace TrueMiningDesktop.User
                     User.Payment_CoinsList = up.Payment_CoinsList;
                     User.PayCoin = up.PayCoin;
                     User.Payment_Coin = up.Payment_Coin;
+                    if (up.Payment_Coin == null &&  up.PayCoin != null && up.PayCoin.CoinTicker != null && up.PayCoin.CoinName != null) { User.Payment_Coin = up.PayCoin.CoinTicker + " - " + up.PayCoin.CoinName; }
                     User.Payment_Wallet = up.Payment_Wallet;
                     User.LICENSE_read = up.LICENSE_read;
                 }
@@ -128,6 +129,7 @@ namespace TrueMiningDesktop.User
                         User.Payment_CoinsList = up.Payment_CoinsList;
                         User.PayCoin = up.PayCoin;
                         User.Payment_Coin = up.Payment_Coin;
+                        if (up.Payment_Coin == null && up.PayCoin != null && up.PayCoin.CoinTicker != null && up.PayCoin.CoinName != null) { User.Payment_Coin = up.PayCoin.CoinTicker + " - " + up.PayCoin.CoinName; }
                         User.Payment_Wallet = up.Payment_Wallet;
                         User.LICENSE_read = up.LICENSE_read;
                     }
