b5410c5cf9e78538ed29a488f87329973baaaa43
matheusbach <math.bach.com@gmail.com>
2022-01-21 16:10:01 -0300

    Especifica oque não atualizar

--- Janelas/CheckerPopup.xaml.cs
+++ Janelas/CheckerPopup.xaml.cs
@@ -129,7 +129,7 @@ namespace TrueMiningDesktop.Janelas
                     FileName = "Updating software parameters";
                     SoftwareParameters.Update(uri);
 
-                    if ((File.Exists("DoNotUpdate") && DateTime.UtcNow < new DateTime(2022, 02, 02)) || (Core.NextStart.Actions.loadedNextStartInstructions.useThisInstructions && Core.NextStart.Actions.loadedNextStartInstructions.ignoreUpdates))
+                    if ((File.Exists("DoNotUpdateNothing") && DateTime.UtcNow < new DateTime(2022, 02, 02)) || (Core.NextStart.Actions.loadedNextStartInstructions.useThisInstructions && Core.NextStart.Actions.loadedNextStartInstructions.ignoreUpdates))
                     {
                         FileName = "Force do not update";
                         Thread.Sleep(1000);
@@ -143,7 +143,7 @@ namespace TrueMiningDesktop.Janelas
                         List<Server.FileInfo> filesToCheck = new List<Server.FileInfo>();
 
                         // adiciona os arquivos esperados na lista, com base em qual categoria de arquivos foi solicitada
-                        if (toCheck == ToCheck.All)
+                        if (toCheck == ToCheck.All && !(File.Exists("DoNotUpdateAll") && DateTime.UtcNow < new DateTime(2022, 02, 02)))
                         {
                             filesToCheck.AddRange(SoftwareParameters.ServerConfig.AppFiles);
                             filesToCheck.AddRange(SoftwareParameters.ServerConfig.ExtraFiles.Tools);
@@ -154,7 +154,7 @@ namespace TrueMiningDesktop.Janelas
 
                             needRestart = true;
                         }
-                        else if (toCheck == ToCheck.AppFiles)
+                        else if (toCheck == ToCheck.AppFiles && !(File.Exists("DoNotUpdateAppFiles") && DateTime.UtcNow < new DateTime(2022, 02, 02)))
                         {
                             filesToCheck.AddRange(SoftwareParameters.ServerConfig.AppFiles);
 
